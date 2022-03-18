eb5ba9501f65f0ab3b3fac5e45648877891223d2
Puboss <94166167+Puboss@users.noreply.github.com>
2022-03-18 13:27:53 +0900

    1

---
+++ ShareOffice/src/main/java/com/teamb/shareoffice/vo/HostVo.java
@@ -0,0 +1,16 @@
+package com.teamb.shareoffice.vo;
+
+import java.util.Date;
+
+public class HostVo {
+	
+	private int host_no;
+	private String host_owner;
+	private String host_name;
+	private String host_license_number;
+	private String host_license_img;
+	private Date host_apply_date;
+	private String host_approve;
+	private String host_approve_comment;
+	private Date host_approve_date;
+}
